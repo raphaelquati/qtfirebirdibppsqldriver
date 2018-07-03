59abcb3ca00fc91a785f740922109708a59ce27d
grigorii <grishas@i.ua>
2018-07-03 11:22:13 +0300

    fixing duplicate aliases (id, id, id -> id, id1, id2)

--- src/qsql_ibpp.cpp
+++ src/qsql_ibpp.cpp
@@ -920,7 +920,16 @@ QSqlRecord QFBResult::record() const
     for (int i = 1; i <= cols; ++i)
     {
 
-        QSqlField f(QString::fromLatin1(rp->iSt->ColumnAlias(i)).simplified(),
+        const QString& column_alias = QString::fromLatin1(rp->iSt->ColumnAlias(i)).simplified();
+        QString alias = column_alias;
+
+        int num(1);
+        while (rec.indexOf(alias) >= 0) {
+            alias = column_alias + QString::number(num);
+            num++;
+        }
+
+        QSqlField f(alias,
                     qIBPPTypeName(rp->iSt->ColumnType(i)));
         f.setLength(rp->iSt->ColumnSize(i));
         f.setPrecision(rp->iSt->ColumnScale(i));
